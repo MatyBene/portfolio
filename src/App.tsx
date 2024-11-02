4f3c3ef55d2bd037a9b274b5702d13ee36467c34
MatyBene <benedettimaty@gmail.com>
2024-11-02 16:23:35 -0300

    agrega About.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Hero } from "./components/Hero";
+import { About } from "./components/About";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Hero />
+      <main>
+        <About />
+      </main>
     </div>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function About() {
+  return (
+    <section>
+      <h2>Sobre mi</h2>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+          consectetur libero. Libero beatae harum, velit error quasi ipsum
+          facilis, corrupti at doloremque nam magni unde sunt praesentium fugiat
+          minima animi?
+        </p>
+      </div>
+    </section>
+  );
+}
