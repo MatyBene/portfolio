b43931b414ad08820c2509a3ebc48b8c651a6ee3
MatyBene <benedettimaty@gmail.com>
2024-11-10 11:30:02 -0300

    agrega index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { Hero } from "./components/Hero";
-import { About } from "./components/About";
+import { SkillsIcons } from "./components/SkillsIcons";
 import { Experience } from "./components/Experience";
+import { Projects } from "./components/Projects";
+import { Education } from "./components/Education";
+import { About } from "./components/About";
 import "./App.css";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("experience");
+
   return (
     <div>
-      <Hero />
-      <main>
-        <About />
-        <Experience />
-      </main>
+      <div>
+        <Hero />
+        <SkillsIcons />
+      </div>
+      <div>
+        <div>
+          <button>Experiencia</button>
+          <button>Proyectos</button>
+          <button>Formación</button>
+          <button>Sobre mí</button>
+        </div>
+
+        {activeTab === "experience" && <Experience />}
+        {activeTab === "projects" && <Projects />}
+        {activeTab === "education" && <Education />}
+        {activeTab === "about" && <About />}
+      </div>
     </div>
   );
 }
