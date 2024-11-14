f04debd6e7b5e43449f1b7816caf2d6055bce740
MatyBene <benedettimaty@gmail.com>
2024-11-14 19:06:02 -0300

    modifica texto

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,24 +4,19 @@ export function About() {
       <div className="scrollable">
         <div className="experience-item">
           <p className="inter">
-            ¡Hola! Soy <strong>Matías Benedetti</strong>, estudiante de la
-            <strong> Tecnicatura Universitaria en Programación</strong> en la
-            <em> Universidad Tecnológica Nacional</em>. Además, tengo aprobado
-            el
-            <strong> 50% de las materias</strong> de la carrera de
-            <strong> Ingeniería Eléctrica</strong> en la
-            <em> Universidad Nacional de Mar del Plata</em>, lo que me brinda
-            una comprensión adicional de la tecnología y la lógica aplicada. He
-            completado el curso de
-            <strong> Desarrollador Web Full Stack </strong>
-            en <em>Digital House</em>, lo que me permitió adquirir conocimientos
-            sólidos en desarrollo web. También tengo interés en el área de
-            <strong> testing</strong>, habiendo trabajado con herramientas como
-            <strong> JUnit</strong> y <strong>Jest </strong>
-            para asegurar la <em>calidad del software</em>. Estoy en
-            <em> constante aprendizaje</em> y me apasiona resolver problemas
-            mediante la programación, enfocándome en el desarrollo de
-            <em> soluciones prácticas y eficientes</em>.
+            ¡Hola! Soy Matías Benedetti, estudiante de la Tecnicatura
+            Universitaria en Programación en la Universidad Tecnológica
+            Nacional. Además, tengo aprobado el 50% de las materias de la
+            carrera de Ingeniería Eléctrica en la Universidad Nacional de Mar
+            del Plata, lo que me brinda una comprensión adicional de la
+            tecnología y la lógica aplicada. He completado el curso de
+            Desarrollador Web Full Stack en Digital House, lo que me permitió
+            adquirir conocimientos sólidos en desarrollo web. También tengo
+            interés en el área de testing, habiendo trabajado con herramientas
+            como JUnit y Jest para asegurar la calidad del software. Estoy en
+            constante aprendizaje y me apasiona resolver problemas mediante la
+            programación, enfocándome en el desarrollo de soluciones prácticas y
+            eficientes.
           </p>
         </div>
       </div>
